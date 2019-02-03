966d91f02bf922b4ef44702925061509e7031dce
Svetlana Tislenko <contextnorm@gmail.com>
2019-02-03 12:38:55 +0200

    fixed output temperature | transition

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
     $(document).ready(() => {
 
         const $map = $('.location__map');
-        const map = new google.maps.Map (
+        const map = new google.maps.Map(
             $map[0], {
                 center: {lat: 44.397, lng: 150.644},
                 zoom: 3,
@@ -12,9 +12,9 @@
 
         const iw = new google.maps.InfoWindow;
 
-        $.getJSON('data/location.json',(data) => {
-            let status = '<ul class="tile-cities">' ;
-            $.each(data,(index, a) => {
+        $.getJSON('data/location.json', (data) => {
+            let status = '<ul class="tile-cities">';
+            $.each(data, (index, a) => {
                 status += '<li class="tile-city">' + '<h3 class="tile-city-name">' + a.city + '</h3>';
                 status += '<span class="tile-city-temperature">' + a.temp + '</span>';
                 status += '<i class="wi">' + a.unicode + '</i>';
@@ -25,22 +25,23 @@
                     size: new google.maps.Size(20, 32)
                 };
                 const marker = new google.maps.Marker({
-                                        position: {lat: a.lat, lng: a.lng},
-                                        map: map,
-                                        icon: image,
-                                        label: {
-                                        color: '#1e4bff',
-                                        fontFamily: 'weathericons',
-                                        fontSize: '35px',
-                                        text: a.unicode }
-                                    });
-                                    marker.addListener('click', () => {
-                                        iw.setContent(a.temp);
-                                        iw.open(map,marker);
-                                    });
+                    position: {lat: a.lat, lng: a.lng},
+                    map: map,
+                    icon: image,
+                    label: {
+                        color: '#1e4bff',
+                        fontFamily: 'weathericons',
+                        fontSize: '35px',
+                        text: a.unicode
+                    }
+                });
+                marker.addListener('click', () => {
+                    iw.setContent(`${a.city}: ${a.temp}&deg;C`);
+                    iw.open(map, marker);
+                });
             });
             status += '</ul>';
             $('#tile').html(status);
         });
     });
-}) (jQuery);
+})(jQuery);
